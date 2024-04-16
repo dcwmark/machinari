50c1272500b41acded6cdab2e416abd455c723fe
Entaro Adun <dcwmark@gmail.com>
2024-04-15 20:49:51 -0400

    simplified fib

--- WebAssembly/AssemblyScript/assembly/asFib.ts
+++ WebAssembly/AssemblyScript/assembly/asFib.ts
@@ -3,13 +3,11 @@
 'use strict';
 
 const aFib: i64[] = [];
-function fibIterRecursive(n: i64, a: i64, b: i64): i64[] {
-  if (n === 0) {
-    aFib.push(b);
-    return aFib;
-  } else {
-    aFib.push(b);
-    return fibIterRecursive(n - 1, a + b, a);
+function fibIterRecursive(n: i64, a: i64, b: i64): void {
+  aFib.push(b);
+  aFib.push(a);
+  while(aFib.length < n) {
+    aFib.push(aFib[aFib.length - 1] + aFib[aFib.length - 2]);
   }
 }
 
