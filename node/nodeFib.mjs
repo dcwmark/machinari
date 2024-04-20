3149aed0ba86857fa942a52ea4eec01757e333a0
Entaro Adun <dcwmark@gmail.com>
2024-04-19 20:54:13 -0400

    node aFib write to file

--- node/nodeFib.mjs
+++ node/nodeFib.mjs
@@ -2,8 +2,10 @@
 
 'use strict';
 
-import fs from 'fs';
 import 'dotenv/config';
+import { spawn } from 'child_process';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 
 const aFib = [];
 const fibIterate = (n, a = 1, b = 0) => {
@@ -24,13 +26,28 @@ const tallyHo = `${process.argv[1]} (${fibIter})`;
 console.time(`${tallyHo}`);
 fib(fibIter);
 console.timeEnd(`${tallyHo}`);
-// console.log(aFib.join(', '));
+
+console.log(`.env fibIter:: ${fibIter} -vs- aFib size::${aFib.length}`);
+
 const arrStr = aFib.map(each => each.toString());
-fs.writeFile('nodeFib.txt', arrStr.join('\n'), (err) => {
+const filePath = fileURLToPath(import.meta.url).split('/');
+const fileName = filePath[filePath.length - 1];
+const outFileName = `${fileName.split('.')[0]}.txt`;
+fs.writeFileSync(outFileName, arrStr.join('\n'), (err) => {
   if (err) {
     console.error(`Error in writing file `, err);
   } else {
-    console.log('Done!');
+    console.log(`File out:: ${outFileName}`);
   }
 });
 
+const wc = spawn('wc', ['-l', outFileName]);
+wc.stdout.on('data', (data) => {
+  console.log(`wc:stdout -l ${data}`);
+});
+wc.stderr.on('data', (data) => {
+  console.error(`wc:stderr -l ${data}`);
+});
+wc.on('close', (code) => {
+  console.log(`wc -l exited with code ${code}`);
+});
--- node/wasmFib.mjs
+++ node/wasmFib.mjs
@@ -2,8 +2,10 @@
 
 'use strict';
 
-import fs from 'fs';
 import 'dotenv/config';
+import { spawn } from 'child_process';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 
 import { asFib } from '../WebAssembly/AssemblyScript/build/release.js';
 
@@ -15,15 +17,32 @@ const intFibIter = parseInt(process.env.fibIter);
 const fibIter = BigInt(intFibIter);
 const tallyHo = `${process.argv[1]} (${fibIter})`;
 console.time(`${tallyHo}`);
-const result = fib(fibIter);
+const aFib = fib(fibIter);
 console.log('\n');
 console.timeEnd(`${tallyHo}`);
-// console.log(result.join(', '));
-const arrStr = result.map(each => each.toString());
-fs.writeFile('wasmFib.txt', arrStr.join('\n'), (err) => {
+
+
+console.log(`.env fibIter:: ${fibIter} -vs- aFib size::${aFib.length}`);
+
+const arrStr = aFib.map(each => each.toString());
+const filePath = fileURLToPath(import.meta.url).split('/');
+const fileName = filePath[filePath.length - 1];
+const outFileName = `${fileName.split('.')[0]}.txt`;
+fs.writeFileSync(outFileName, arrStr.join('\n'), (err) => {
   if (err) {
     console.error(`Error in writing file `, err);
   } else {
-    console.log('Done!');
+    console.log(`File out:: ${outFileName}`);
   }
+});
+
+const wc = spawn('wc', ['-l', outFileName]);
+wc.stdout.on('data', (data) => {
+  console.log(`wc:stdout -l ${data}`);
+});
+wc.stderr.on('data', (data) => {
+  console.error(`wc:stderr -l ${data}`);
+});
+wc.on('close', (code) => {
+  console.log(`wc -l exited with code ${code}`);
 });
