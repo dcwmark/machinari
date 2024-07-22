dae34e09ca0ab9dc79fa1ef9ca9e4363b1530426
Entaro Adun <dcwmark@gmail.com>
2024-07-22 15:59:49 -0400

    recode large number addition

--- WebAssembly/AssemblyScript/assembly/helper.ts
+++ WebAssembly/AssemblyScript/assembly/helper.ts
@@ -5,7 +5,7 @@
 const NINE_DIGIT_CHUNK = 1_000_000_000;
 
 const largeNumberToI32Array = (numStr: string): i32[] => {
-  console.log(`### largeNumberToI32Array ... ${numStr}`);
+  console.log(`### largeNumberToI32Array ... ${numStr} length::${numStr.length}`);
   const CHUNK_SIZE: i32 = 9;
   const result: i32[] = [];
   
@@ -14,7 +14,7 @@ const largeNumberToI32Array = (numStr: string): i32[] => {
     if (start < 0) start = 0;
     let chunkStr: string = numStr.substring(start, i);
     let chunkValue: i32 = (i32)(parseInt(chunkStr));
-    result.unshift(chunkValue);
+    result.push(chunkValue);
   }
 
   console.log(`========== result::${result}`);
@@ -30,11 +30,17 @@ const arrayAddition = (num1: i32[], num2: i32[]): i32[] => {
   const result: i32[] = new Array<i32>(length).fill(0);
   let carry: i32 = 0;
 
+  console.log(`***** length::${length}`);
   for (let i: i32 = 0; i < length; i++) {
-    console.log(`***** num1[${i}]::${num1[i]}]`);
-    console.log(`***** num2[${i}]::${num2[i]}]`);
-    console.log(`***** carry::${carry}]`);
-    const sum: i64 = (num1[i] || 0) as i64 + (num2[i] || 0) as i64 + carry;
+    console.log(`***** i::${i}`);
+    console.log(`***** num1 length::${num1.length}`);
+    const numA: i64 = num1.length <= i ? 0 : num1[i]; 
+    console.log(`***** numA::${numA}`);
+    console.log(`***** num2 length::${num2.length}`);
+    const numB: i64 = num2.length <= i ? 0 : num2[i]; 
+    console.log(`***** numB::${numB}`);
+    // console.log(`***** carry::${carry}`);
+    const sum: i64 = numA + numB + carry;
     console.log(`***** sum::${sum}`);
     result[i] = i32(sum % NINE_DIGIT_CHUNK);
     console.log(`***** result[${i}]::${result[i]}`);
@@ -51,25 +57,27 @@ const arrayAddition = (num1: i32[], num2: i32[]): i32[] => {
 }; 
 
 const i32ArrayToString = (i32Array: i32[]): string => {
-  console.log(`### i32ArrayToString ...`);
+  console.log(`### i32ArrayToString ... i32Array::${i32Array} length::${i32Array.length}`);
+  const length: i32 = i32Array.length;
+  console.log(`***** length::${length}`);
   let result: string = '';
-  /**
-   * ========================================================
-   * AssemblyScript has not implement closure yet. 07/18/2024
-   * --------------------------------------------------------
-      i32Array.map((each: i32) => result += each.toString());
-   */
-  for (let i: i32 = 0; i < i32Array.length; i++) {
+
+  // for (let i: i32 = 0; i < length; i++) {
+  for (let i: i32 = length - 1; i >= 0; i--) {
+    console.log(`***** i:: ${i}`);
+    console.log(`********** i32Array[${i}]:: ${i32Array[i]}`);
     result += i32Array[i].toString();
+    console.log(`********** result:: ${result}`);
   };
-  console.log(`********** result::${result}`);
   return result;
 };
 
 export const largeNumberAddition = (a: string, b: string): string => {
   console.log(`### largeNumberAddition ...`);
   const aa = largeNumberToI32Array(a);
+  console.log(`++++++++++ aa:: ${aa}`);
   const bb = largeNumberToI32Array(b);
+  console.log(`++++++++++ bb:: ${bb}`);
   const arraySum: i32[] = arrayAddition(aa, bb);
   console.log(`++++++++++ ${aa} + ${bb} = ${arraySum}`);
   return i32ArrayToString(arraySum);
